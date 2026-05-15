0def913beb8148cef96fd4e98e5055dc0e349515
Liliya <78988408+lpanfilova@users.noreply.github.com>
2026-05-15 17:07:22 -0400

    added SumAllTails & TestSumAllTails to /arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -21,3 +21,30 @@ func SumSlice(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, SumSlice(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+
+		if len(numbers) > 0 {
+			tail := numbers[1:]
+			sums = append(sums, SumSlice(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+
+	}
+
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,8 +1,19 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
+	assertCorrectResult := func(t testing.TB, got, want int) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
 	t.Run("array of 5 numbers", func(t *testing.T) {
 		numbers := [5]int{1, 2, 3, 4, 5}
 
@@ -23,10 +34,36 @@ func TestSum(t *testing.T) {
 
 }
 
-func assertCorrectResult(t testing.TB, got, want int) {
-	t.Helper()
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("sum tails of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+
 }
